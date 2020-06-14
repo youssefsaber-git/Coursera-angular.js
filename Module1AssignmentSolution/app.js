2fe55aa397ef8f57e6594397b640ac54976925b4
youssef <youssefe54@gmail.com>
2020-06-14 22:57:42 +0100

    assignment 1 finished

--- Module1AssignmentSolution/app.js
+++ Module1AssignmentSolution/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     angular.module('LunchCheck', [])
-        .controller('LunchCheckController',LunchCheckController )
+        .controller('LunchCheckController', LunchCheckController);
         LunchCheckController.$inject = ['$scope'];
         function LunchCheckController($scope) {
             $scope.inputstring = "";
