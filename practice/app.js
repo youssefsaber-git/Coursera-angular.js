12d0ba716bd36a8c216b55137bf77987f6cced6d
youssef <youssefe54@gmail.com>
2020-08-03 17:03:51 +0100

    x

--- practice/app.js
+++ practice/app.js
@@ -1,14 +1,24 @@
 // JavaScript source code
 (function () {
     'use strict';
-    angular.module('exApp', [])
+    angular.module('exApp', []).controller('itemctrl',itemctrl)
         .directive('example', exampleDirective);
     function exampleDirective() {
         var ddo = {
             restrict: "AE",
-            templateUrl: 'example.html'
+            templateUrl: 'example.html',
+            scope: {
+                menu: '<items',
+                onRemove: '&'
+            },
         };
         return ddo;
+    }
+    function itemctrl() {
+        var item = this;
+        item.name = "";
+        item.description = "";
+
     }
 }
 )();
