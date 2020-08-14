123a110c64137bf730194ac2b0009e4047b5a968
youssef <youssefe54@gmail.com>
2020-08-14 17:13:20 +0100

    x

--- m4a/src/found.component.js
+++ m4a/src/found.component.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
     angular.module('MenuApp').
-        component('found', {
+        component('foundtemplate', {
             TemplateUrl: 'src/templates/found.template.html',
           //  controller: foundComponentController,
           //  bindings: {
